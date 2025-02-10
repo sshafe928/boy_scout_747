da50a789236aa7dafab171f85be52c22135378ec
NoelleWeaver <141285591+NoelleWeaver@users.noreply.github.com>
2025-02-10 14:15:15 -0700

    Added a background to the calendar page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './Home';
+import Calendar from './CalendarPage';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const router = createBrowserRouter([
   { path: '/', element: <Home />},
+  { path: '/calendar', element: <Calendar />}
 ])
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
