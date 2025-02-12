a7e39a18b55c5635254d0a4087a0b6f220d8a079
Nico <141285419+red4cted25@users.noreply.github.com>
2025-02-12 13:33:40 -0700

    Complete Footer
    Footer spacing is fully complete on desktop, and no further styling is needed currently

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -23,7 +23,7 @@ const Footer = () => {
                 <footer className="font-Inter w-screen">
                     {/* Upper Section */}
                     <div className="bg-brand-primary-black justify-center text-center pt-6 shadow-defaultShadow">
-                        <h1 className="text-brand-primary-gold font-bold text-3xl ">TROOP 747</h1>
+                        <h1 className="text-brand-primary-gold font-bold text-3xl">TROOP 747</h1>
                         <hr className="h-0.5 border-none bg-brand-primary-gold w-4/5 mx-auto mb-4" />
                         <div className="flex flex-wrap text-brand-primary-gold gap-8 w-4/5 mx-auto justify-center font-bold">
                             <Link to="/" className="text-gray-800 text-lg font-medium">Home</Link>
@@ -52,27 +52,37 @@ const Footer = () => {
                 ) : (
                     <footer className="font-Inter w-screen">
                         {/* Upper Section */}
-                            <section className="flex justify-around bg-brand-primary-black text-center py-6 shadow-defaultShadow">
+                            <section className="flex justify-around bg-brand-primary-black text-center pt-12 pb-4 shadow-defaultShadow">
                                 {/* Left Section */}
                                 <div className="flex justify-center items-center text-center text-brand-primary-gold w-1/3">
-                                    <p className='w-3/4'>Meeting every Tuesday at 6:30 PM at Phoenix Friends Church, 8055 N 39th Ave, Phoenix, AZ 85051</p>
+                                    <p className='w-3/4 leading-10'>Meeting every Tuesday at 6:30 PM at Phoenix Friends Church, 8055 N 39th Ave, Phoenix, AZ 85051</p>
                                 </div>
                                 {/* Middle Section */}
                                 <div className="w-1/3 flex flex-col items-center">
-                                    <h1 className="text-brand-primary-gold font-bold text-2xl ">TROOP 747</h1>
+                                    <h1 className="text-brand-primary-gold font-bold text-2xl mb-2">TROOP 747</h1>
                                     <img src={LogoYellow} alt="Gold Troop Logo"/>
                                 </div>
                                 {/* Right Section */}
-                                <div className="w-1/3 flex flex-col items-center font-bold">
-                                    <h2 className="text-white text-xl">Explore</h2>
+                                <div className="w-1/3 flex flex-col items-center justify-center font-bold">
+                                    <h2 className="text-white text-xl text-left mb-2 -ml-[5%]">Explore</h2>
                                     <ul className='text-brand-primary-gold text-left'>
-                                        <li><NavLink title={'Home'} href={'/'} /></li>
-                                        <li><NavLink title={"Eagle's Nest"} href={'/eagles-nest'} /></li>
-                                        <li><NavLink title={'News'} href={'/news'} /></li>
-                                        <li><NavLink title={'Calendar'} href={'/calendar'} /></li>
-                                        <li><NavLink title={'Photos'} href={'/photos'} /></li>
+                                        <li className='mb-2'><NavLink title={'Home'} href={'/'} /></li>
+                                        <li className='mb-2'><NavLink title={"Eagle's Nest"} href={'/eagles-nest'} /></li>
+                                        <li className='mb-2'><NavLink title={'News'} href={'/news'} /></li>
+                                        <li className='mb-2'><NavLink title={'Calendar'} href={'/calendar'} /></li>
+                                        <li className='mb-2'><NavLink title={'Photos'} href={'/photos'} /></li>
                                         <li><NavLink title={'For Admins'} href={'/admin'} /></li>
                                     </ul>
+                                </div>
+                            </section>
+                            <section className='bg-brand-primary-black flex justify-center pb-4'>
+                                <div className="mx-auto flex items-center gap-4">
+                                    <a href="https://www.instagram.com/troop747phoenix/" target="_blank" rel="noopener noreferrer">
+                                        <LuInstagram className="text-brand-primary-gold size-7"/>
+                                    </a>
+                                    <a href="https://www.facebook.com/troop747phoenix" target="_blank" rel="noopener noreferrer">
+                                        <LuFacebook className="text-brand-primary-gold size-7"/>
+                                    </a>
                                 </div>
                             </section>
                         {/* Lower Section */}
