84cb4535068e90644fa596060ce2cb31e6ba0d04
hey.its.a <141285328+ageeha927@users.noreply.github.com>
2025-02-12 14:41:48 -0700

    Home Section 1 and Header fixing

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,9 +1,11 @@
 import Header from './components/Header';
+import HomeSection1 from './components/HomeSection1';
 
 function Home() {
     return (
         <>
           <Header/>
+          <HomeSection1/>
         </>
     );
 }
