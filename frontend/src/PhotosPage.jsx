86b55dfbcabac24032d43c96d93be8e7d46044a2
Sabrina Shafer <141285407+sshafe928@users.noreply.github.com>
2025-02-25 15:07:29 -0700

    header and footer consistency

--- frontend/src/PhotosPage.jsx
+++ frontend/src/PhotosPage.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header';
 import Gallery from './components/Gallery'
+import Footer from './components/Footer';
 
 function Home() {
     return (
         <>
             <Header/>
             <Gallery/>
+            <Footer/>
         </>
     );
 }
