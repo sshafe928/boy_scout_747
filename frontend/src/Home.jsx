f134da80e70794926f4865edf35dbe989c7536e2
Diego-Esquivias <desqui048@west-mec.org>
2025-02-25 15:21:44 -0700

    finished mobile version of home section 2 and working on desktop

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,10 +1,12 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeSection2 from './components/HomeSection2';
 
 function Home() {
     return (
         <>
           <Header/>
+          <HomeSection2/>
           <Footer/>
         </>
     );
