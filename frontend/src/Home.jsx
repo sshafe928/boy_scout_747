55b06749e418792a49b7d78302ea6e75640b019c
Nico <141285419+red4cted25@users.noreply.github.com>
2025-02-25 15:26:15 -0700

    Improved Home Section 3
    Improved the home section 3 and created the events component for events to be mapped onto the page using database info

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,10 +1,13 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeSection3 from './components/HomeSection3';
 
 function Home() {
     return (
         <>
           <Header/>
+          <HomeSection3/>
+          <div className="h-[150vh]"></div>
           <Footer/>
         </>
     );
---
+++ frontend/src/components/HomeEvent.jsx
@@ -0,0 +1,15 @@
+const HomeEvent = ({img, title, desc}) => {
+    return (
+        <div className="flex flex-col bg-brand-primary-black w-4/5 rounded-2xl">
+            {/* Top Section */}
+            <img src={img} alt={title} className="rounded-t-2xl"/>
+            {/* Bottom Section */}
+            <div className="p-6">
+                <h1 className="text-brand-primary-gold">{title}</h1>
+                <p className="text-white line-clamp-7">{desc}</p>
+            </div>
+        </div>
+    )
+}
+
+export default HomeEvent
