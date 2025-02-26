2e3df780ff10456cc70962e948507b11e3dd295f
Nico <141285419+red4cted25@users.noreply.github.com>
2025-02-26 15:22:50 -0700

    Completed Home Section 3
    Home section 3 done with svg background and events made to be filled in from database info later

--- frontend/src/components/HomeSection3.jsx
+++ frontend/src/components/HomeSection3.jsx
@@ -15,24 +15,32 @@ const HomeSection3 = () => {
         }, []);
         
         const events = [
-            { id: 1, src: 'https://res.cloudinary.com/drnaycy06/image/upload/v1731009869/cld-sample-2.jpg', title: 'Event 1', desc: 'Description of Event 1' },
+            { id: 1, src: 'https://res.cloudinary.com/drnaycy06/image/upload/v1731009869/cld-sample-2.jpg', title: 'Camp Geronimo', desc: 'Camp Geronimo is a premier summer camp experience for Boy Scouts of America, nestled in the scenic Payson, Arizona wilderness. Surrounded by towering ponderosa pines and breathtaking mountain landscapes' },
             { id: 2, src: 'https://res.cloudinary.com/drnaycy06/image/upload/v1731009869/cld-sample.jpg', title: 'Event 2', desc: 'Description of Event 2' },
             { id: 3, src: 'https://res.cloudinary.com/drnaycy06/image/upload/v1731009868/samples/coffee.jpg', title: 'Event 3', desc: 'Description of Event 3' }
         ]
     return (
         <>
             {isMobile ? (
-                <>
-                    <h1 className="font-Tienne text-2xl">Upcoming Events</h1>
+                <section className='py-8 bg-brand-primary-brown' style={{backgroundImage: 'url(https://res.cloudinary.com/dmrevelyc/image/upload/v1740607699/Animated_Shape_3_vagk64.svg)', backgroundSize: 'cover', backgroundPosition: 'center'}}>
+                    <h1 className="font-Tienne text-2xl text-center mb-4">Upcoming Events</h1>
                     {/* Event Cards */}
                     <div className="flex gap-6 justify-center flex-wrap">
                         {events.map((event) => {
                             return <HomeEvent key={event.id} img={event.src} title={event.title} desc={event.desc}/>
                         })}
                     </div>
-                </>
+                </section>
             ) : (
-                <h1 className="font-Tienne text-4xl">Upcoming Events</h1>
+                <section className='py-8' style={{backgroundImage: 'url(https://res.cloudinary.com/dmrevelyc/image/upload/v1740607699/Animated_Shape_3_vagk64.svg)', backgroundSize: 'cover', backgroundPosition: 'center'}}>
+                    <h1 className="font-Tienne text-4xl text-center mb-4">Upcoming Events</h1>
+                    {/* Event Cards */}
+                    <div className="flex gap-6 justify-center flex-wrap">
+                        {events.map((event) => {
+                            return <HomeEvent key={event.id} img={event.src} title={event.title} desc={event.desc}/>
+                        })}
+                    </div>
+                </section>
             )}
         </>
     )
