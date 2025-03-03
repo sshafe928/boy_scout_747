9d02dc21802638adc82ad4159489c3270815fd3d
Nico <141285419+red4cted25@users.noreply.github.com>
2025-03-03 14:42:38 -0700

    Converted isMobile to Responsive Design
    Converted all isMobile functionality to responsive design to ensure speed, loading, and correct responsiveness between device sizes. Works on mobile, tablet, mobile/tablet landscape, and desktop

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,98 +1,84 @@
-import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { LuFacebook, LuInstagram  } from "react-icons/lu";
-import NavLink from './NavLink'
+import { LuFacebook, LuInstagram } from "react-icons/lu";
 import LogoYellow from "../assets/LogoYellow.png"
 
 const Footer = () => {
-        const [isMobile, setIsMobile] = useState(false);
-    
-        useEffect(() => {
-            const handleResize = () => {
-                setIsMobile(window.innerWidth <= 768);
-            };
-            
-            handleResize();
-            window.addEventListener('resize', handleResize);
-            return () => window.removeEventListener('resize', handleResize);
-        }, []);
-
     return (
-        <>
-            {isMobile ? (
-                <footer className="font-Inter w-screen">
-                    {/* Upper Section */}
-                    <div className="flex flex-col bg-brand-primary-black justify-center text-center pt-6 shadow-xl md:flex-row md:justify-around md:pt-12 md:pb-4">
-                        <h1 className="text-brand-primary-gold font-bold text-3xl">TROOP 747</h1>
-                        <hr className="h-0.5 border-none bg-brand-primary-gold w-4/5 mx-auto mb-4 md:hidden" />
-                        <div className="flex flex-wrap text-brand-primary-gold gap-8 w-4/5 mx-auto justify-center font-bold">
-                            <Link to="/" className="text-gray-800 text-lg font-medium">Home</Link>
-                            <Link to="/eagles-nest" className="text-gray-800 text-lg font-medium">Eagle's Nest</Link>
-                            <Link to="/news" className="text-gray-800 text-lg font-medium">News</Link>
-                            <Link to="/calendar" className="text-gray-800 text-lg font-medium">Calendar</Link>
-                            <Link to="/photos" className="text-gray-800 text-lg font-medium">Photos</Link>
-                            <Link to="/admins" className="text-gray-800 text-lg font-medium">For Admins</Link>
-                        </div>
-                        <p className="text-center text-brand-primary-gold text-xs mt-6 mb-4 w-4/5 mx-auto">Meeting every Tuesday at 6:30 PM at Phoenix Friends Church, 8055 N 39th Ave, Phoenix, AZ 85051</p>
-                        {/* Social Icons */}
-                        <div className="flex justify-center pb-6">
-                            <a href="https://www.facebook.com/troop747phoenix" target="_blank" rel="noopener noreferrer">
-                                <LuFacebook className="text-brand-primary-gold size-7"/>
-                            </a>
-                            <a href="https://www.instagram.com/troop747phoenix/" target="_blank" rel="noopener noreferrer">
-                                <LuInstagram className="text-brand-primary-gold size-7"/>
-                            </a>
-                        </div>
-                    </div>
-                    {/* Lower Section */}
-                    <div className="bg-brand-primary-gold font-Tienne">
-                        <p className="text-center text-brand-primary-black py-4">© 2025 Copyright: Boy Scouts 747. All rights reserved.</p>
-                    </div>
-                </footer>
-                ) : (
-                    <footer className="font-Inter w-screen">
-                        {/* Upper Section */}
-                            <section className="flex justify-around bg-brand-primary-black text-center pt-12 pb-4 shadow-defaultShadow">
-                                {/* Left Section */}
-                                <div className="flex justify-center items-center text-center text-brand-primary-gold w-1/3">
-                                    <p className='w-3/4 leading-10'>Meeting every Tuesday at 6:30 PM at Phoenix Friends Church, 8055 N 39th Ave, Phoenix, AZ 85051</p>
-                                </div>
-                                {/* Middle Section */}
-                                <div className="w-1/3 flex flex-col items-center">
-                                    <h1 className="text-brand-primary-gold font-bold text-2xl mb-2">TROOP 747</h1>
-                                    <img src={LogoYellow} alt="Gold Troop Logo"/>
-                                </div>
-                                {/* Right Section */}
-                                <div className="w-1/3 flex flex-col items-center justify-center font-bold">
-                                    <h2 className="text-white text-xl text-left mb-2 -ml-[5%]">Explore</h2>
-                                    <ul className='text-brand-primary-gold text-left'>
-                                        <li className='mb-2'><NavLink title={'Home'} href={'/'} /></li>
-                                        <li className='mb-2'><NavLink title={"Eagle's Nest"} href={'/eagles-nest'} /></li>
-                                        <li className='mb-2'><NavLink title={'News'} href={'/news'} /></li>
-                                        <li className='mb-2'><NavLink title={'Calendar'} href={'/calendar'} /></li>
-                                        <li className='mb-2'><NavLink title={'Photos'} href={'/photos'} /></li>
-                                        <li><NavLink title={'For Admins'} href={'/admin'} /></li>
-                                    </ul>
-                                </div>
-                            </section>
-                            <section className='bg-brand-primary-black flex justify-center pb-4'>
-                                <div className="mx-auto flex items-center gap-4">
-                                    <a href="https://www.instagram.com/troop747phoenix/" target="_blank" rel="noopener noreferrer">
-                                        <LuInstagram className="text-brand-primary-gold size-7"/>
-                                    </a>
-                                    <a href="https://www.facebook.com/troop747phoenix" target="_blank" rel="noopener noreferrer">
-                                        <LuFacebook className="text-brand-primary-gold size-7"/>
-                                    </a>
-                                </div>
-                            </section>
-                        {/* Lower Section */}
-                        <section className="bg-brand-primary-gold font-Tienne">
-                            <p className="text-center text-brand-primary-black py-4">© 2025 Copyright: Boy Scouts 747. All rights reserved.</p>
-                        </section>
-                    </footer>
-                )
-        }
-        </>
+        <footer className="font-Inter w-full bg-brand-primary-black">
+            {/* Upper Section - Mobile First Design */}
+            <div className="pt-6 flex flex-col items-center text-center md:pt-12">
+                <h1 className="text-brand-primary-gold font-bold text-3xl md:text-2xl">TROOP 747</h1>
+                <hr className="h-0.5 border-none bg-brand-primary-gold w-4/5 mx-auto mb-4 md:hidden" />
+            </div>
+            <div className="flex flex-col justify-center text-center shadow-xl md:flex-row md:justify-around md:pb-4">
+                {/* Navigation Links - Visible on mobile */}
+                <div className="flex flex-wrap text-brand-primary-gold gap-8 w-4/5 mx-auto justify-center font-medium text-lg md:hidden">
+                    <Link to="/">Home</Link>
+                    <Link to="/eagles-nest">Eagle's Nest</Link>
+                    <Link to="/news">News</Link>
+                    <Link to="/calendar">Calendar</Link>
+                    <Link to="/photos">Photos</Link>
+                    <Link to="/admins">For Admins</Link>
+                </div>
+                
+                {/* Meeting Info - Different position based on screen size */}
+                <p className="text-center text-brand-primary-gold text-xs mt-6 mb-4 w-4/5 mx-auto md:hidden">
+                    Meeting every Tuesday at 6:30 PM at Phoenix Friends Church, 8055 N 39th Ave, Phoenix, AZ 85051
+                </p>
+                
+                {/* Desktop Left Section - Hidden on mobile */}
+                <div className="hidden md:flex md:justify-center md:items-center md:text-center md:text-brand-primary-gold md:w-1/3">
+                    <p className='md:w-3/4 md:leading-10'>
+                        Meeting every Tuesday at 6:30 PM at Phoenix Friends Church, 8055 N 39th Ave, Phoenix, AZ 85051
+                    </p>
+                </div>
+                
+                {/* Desktop Middle Section - Logo visible only on desktop */}
+                <div className="hidden md:w-1/3 md:flex md:flex-col md:items-center">
+                    <img src={LogoYellow} alt="Gold Troop Logo" className="hidden md:block" />
+                </div>
+                
+                {/* Desktop Right Section - Navigation links */}
+                <div className="hidden md:w-1/3 md:flex md:flex-col md:items-center md:justify-center md:font-bold">
+                    <h2 className="hidden md:block md:text-white md:text-xl md:text-left md:mb-2 md:-ml-[5%]">Explore</h2>
+                    <ul className='hidden md:block md:text-brand-primary-gold md:text-left'>
+                        <li className='mb-2'><Link to="/" className="text-brand-primary-gold">Home</Link></li>
+                        <li className='mb-2'><Link to="/eagles-nest" className="text-brand-primary-gold">Eagle's Nest</Link></li>
+                        <li className='mb-2'><Link to="/news" className="text-brand-primary-gold">News</Link></li>
+                        <li className='mb-2'><Link to="/calendar" className="text-brand-primary-gold">Calendar</Link></li>
+                        <li className='mb-2'><Link to="/photos" className="text-brand-primary-gold">Photos</Link></li>
+                        <li><Link to="/admin" className="text-brand-primary-gold">For Admins</Link></li>
+                    </ul>
+                </div>
+                
+                {/* Social Icons */}
+                <div className="flex justify-center pb-6 md:hidden">
+                    <a href="https://www.facebook.com/troop747phoenix" target="_blank" rel="noopener noreferrer">
+                        <LuFacebook className="text-brand-primary-gold size-7"/>
+                    </a>
+                    <a href="https://www.instagram.com/troop747phoenix/" target="_blank" rel="noopener noreferrer">
+                        <LuInstagram className="text-brand-primary-gold size-7"/>
+                    </a>
+                </div>
+            </div>
+            
+            {/* Social Icons - Desktop */}
+            <div className='hidden md:block md:bg-brand-primary-black md:flex md:justify-center md:pb-4'>
+                <div className="hidden md:mx-auto md:flex md:items-center md:gap-4">
+                    <a href="https://www.instagram.com/troop747phoenix/" target="_blank" rel="noopener noreferrer">
+                        <LuInstagram className="text-brand-primary-gold size-7"/>
+                    </a>
+                    <a href="https://www.facebook.com/troop747phoenix" target="_blank" rel="noopener noreferrer">
+                        <LuFacebook className="text-brand-primary-gold size-7"/>
+                    </a>
+                </div>
+            </div>
+            
+            {/* Lower Section */}
+            <div className="bg-brand-primary-gold font-Tienne">
+                <p className="text-center text-brand-primary-black py-4">© 2025 Copyright: Boy Scouts 747. All rights reserved.</p>
+            </div>
+        </footer>
     )
 }
 
