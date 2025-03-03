548b0c6ac99510863261c0da549bb2b8c09d286e
G00berDev <adeptbee9@gmail.com>
2025-03-03 13:54:31 -0700

    Tailwind fixed and added
    Tailwind Addition

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,26 @@
+
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'brand-primary-black': '#100102',
+        'brand-primary-gold': '#EDC736',
+        'brand-primary-brown': '#784B33',
+        
+        'brand-accent-brown': '#502F18',
+        'brand-accent-warm': '#EEBA00',
+        'brand-accent-light': '#FFE995',
+      },
+      fontFamily: {
+        'Tienne': ['Tienne', 'Times New Roman', 'Times', 'serif', 'system-ui'],
+        'Inter': ['Inter', 'Verdana', 'Geneva', 'Tahoma', 'sans-serif', 'system-ui']
+      },
+      boxShadow: {
+        'defaultShadow': '0, 10, 10, 0, rgb(0, 0, 0)'
+      }
+    },
+  },
+  plugins: [],
+}
