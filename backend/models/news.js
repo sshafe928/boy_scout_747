359a522383390c63609cb053daad7f54ee87b456
Daniel <141285622+DanielFreeman068@users.noreply.github.com>
2025-03-05 14:43:49 -0700

    Update news.js

--- backend/models/news.js
+++ backend/models/news.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose');
 const newsSchema = new mongoose.Schema({
     _id: { type: mongoose.Schema.Types.ObjectId, required: true, auto: true },
     title: { type: String, required: true },
-    description: { type: String },
-    type: { type: String, required: true },
-    img_url: { type: String, required: true},
+    description: { type: String, required: true },
+    month: { type: String, required: true },
+    day: { type: String, required: true },
+    location: { type: String } 
 }, { collection: 'Mock_News' });
 
 module.exports = mongoose.model('News', newsSchema);
