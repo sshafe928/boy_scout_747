03ead8702f2f2f6655337b540e188b155ef308fe
Daniel <141285622+DanielFreeman068@users.noreply.github.com>
2025-03-06 15:19:58 -0700

    eagles backend linked

--- frontend/src/EaglesNest.jsx
+++ frontend/src/EaglesNest.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { useState, useEffect } from 'react';
 
 const EaglesNest = () => {
     const [formattedEagles, setFormattedEagles] = useState([]);
@@ -11,23 +10,24 @@ const EaglesNest = () => {
         .then((res) => res.json())
         .then((data) => {
             if (data.success) {
-                const formatted = data.data.map((eagle) => {
-                    const date = new Date(eagle.date);
+                const formatted = data.data.map((eagles) => {
+                    const date = new Date(eagles.date);
                     const year = date.getFullYear();
                     const month = date.toLocaleString('en-US', { month: 'long' });
                     const day = date.getDate();
 
                     return {
-                        name: eagle.name,
+                        name: eagles.name,
                         year,
                         month,
                         day,
-                        description: eagle.description,
-                        rank: eagle.rank,
-                        img_url: eagle.img_url
+                        description: eagles.description,
+                        rank: eagles.rank,
+                        img_url: eagles.img_url
                     };
                 });
                 setFormattedEagles(formatted);
+                console.log(formattedEagles)
             }
         })
         .catch(error => console.error('Error fetching news:', error));
@@ -36,11 +36,17 @@ const EaglesNest = () => {
         <>
         <Header/>
         <div className="flex flex-col gap-4">
-            {formattedEagles.map((eagle, index) => (
-                <div key={index} className="flex flex-col gap-4">
-                    <h3>{eagle.name}</h3>
-                </div>
-            ))}
+            {formattedEagles.map((eagle, index) => {
+                return(
+                    <div key={index} className="flex flex-col gap-4">
+                        <h3>{eagle.name}</h3>
+                        <h3>{eagle.rank}</h3>
+                        <h3>{eagle.date}</h3>
+                        <h3>{eagle.description}</h3>
+                        <h3>{eagle.img_url}</h3>
+                    </div>
+                )
+            })}
         </div>
         <Footer/>
         </>
