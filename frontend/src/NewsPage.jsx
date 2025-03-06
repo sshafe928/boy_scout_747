466f8d2c1cf578a815f8ad5680987651a545d866
Daniel <141285622+DanielFreeman068@users.noreply.github.com>
2025-03-06 13:52:52 -0700

    working backend for news

--- frontend/src/NewsPage.jsx
+++ frontend/src/NewsPage.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { useState, useEffect } from 'react';
 
 const NewsPage = () => {
     const [formattedEvents, setFormattedEvents] = useState([]);
@@ -13,11 +12,15 @@ const NewsPage = () => {
             if (data.success) {
                 const formatted = data.data.map((event) => {
                     const date = new Date(event.date);
-                    const formattedDate = date.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+                    const year = date.getFullYear();
+                    const month = date.toLocaleString('en-US', { month: 'long' });
+                    const day = date.getDate();
 
                     return {
                         title: event.title,
-                        date: formattedDate,
+                        year,
+                        month,
+                        day,
                         description: event.description,
                         location: event.location,
                     };
@@ -34,20 +37,22 @@ const NewsPage = () => {
         {/* Mobile Version of News Page */}
         <div className='flex flex-col my-5 gap-8 flex-wrap items-center justify-center sm:flex-row'>
             {formattedEvents.map((event, index) => {
-                // Checks if card is odd to determine the background color
                 return (
                     <div key={event.title} className={`font-Tienne p-8 border-b border-gray-300 flex flex-col w-4/5 h-64 sm:w-1/3 lg:w-1/4 ${index % 2 !== 0 ? "bg-brand-accent-light" : "bg-brand-accent-warm text-white"}`}>
-                        <h3 className='text-2xl'>{event.date}</h3>
-                        <h1 className='text-5xl py-4 font-bold'>{event.date}</h1>
+                        <div className="flex justify-between">
+                            <h3 className='text-2xl'>{event.month}</h3>
+                            <h3 className="text-2xl">{event.year}</h3>
+                        </div>
+                        <h1 className='text-5xl py-4 font-bold'>{event.day}</h1>
                         <p className='text-lg pb-4'>{event.title}</p>
                         <p className='font-semibold'>{event.location}</p>
                     </div>
-                )
-                })}
+                );
+            })}
         </div>
         <Footer/>
         </>
-    )
+    );
 }
 
-export default NewsPage
+export default NewsPage;
