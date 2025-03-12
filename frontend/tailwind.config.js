26b4fc2ac7f7e7151e261ef0e5fb31af0424f262
Nico <141285419+red4cted25@users.noreply.github.com>
2025-03-12 15:23:18 -0700

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,18 +3,18 @@ export default {
 content: ["./src/**/*.{html,js,jsx}"],
 theme: {
     extend: {
-    colors: {
-        'brand-primary-black': '#100102',
-        'brand-primary-gold': '#EDC736',
-        'brand-primary-brown': '#784B33',
-        
-        'brand-accent-brown': '#502F18',
-        'brand-accent-brown-light': '#80604f',
-        'brand-accent-warm': '#EEBA00',
-        'brand-accent-light': '#FFE995',
-        'brand-accent-yellow': '#F8F3E1',
-    },
-    fontFamily: {
+      colors: {
+          'brand-primary-black': '#100102',
+          'brand-primary-gold': '#EDC736',
+          'brand-primary-brown': '#784B33',
+          
+          'brand-accent-brown': '#502F18',
+          'brand-accent-brown-light': '#80604f',
+          'brand-accent-warm': '#EEBA00',
+          'brand-accent-light': '#FFE995',
+          'brand-accent-yellow': '#F8F3E1',
+      },
+      fontFamily: {
         'Tienne': ['Tienne', 'Times New Roman', 'Times', 'serif', 'system-ui'],
         'Inter': ['Inter', 'Verdana', 'Geneva', 'Tahoma', 'sans-serif', 'system-ui'],
       },
@@ -26,15 +26,14 @@ theme: {
       },
       blur: {
         xs: '2px',
+      },
+      boxShadow: {
+          'defaultShadow': '0, 10, 10, 0, rgb(0, 0, 0)'
+      },
+      translate: {
+          'neg-full': '-100%'
       }
     },
-    boxShadow: {
-        'defaultShadow': '0, 10, 10, 0, rgb(0, 0, 0)'
-    },
-    translate: {
-        'neg-full': '-100%'
-    }
-    },
-},
-plugins: [],
+  },
+  plugins: [],
 }
